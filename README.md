3489fa9899bbedc901b460be6306bc5f6c6c1db8
knh190 <knh190@hotmail.com>
2025-05-07 22:22:54 -0600

    init.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Doodle Classifier (JS)
+
+This repo is a fork of [the original](https://github.com/ROCCYK/DoodlePredictor/tree/main). It contains converted TensorFlow JS models.
+
+This project uses a **MobileNet** architecture for classifying grayscale images. The model has been optimized to handle greyscale images efficiently, leveraging the depth and speed of MobileNet, which is typically used for lightweight image classification tasks. The project includes a data pipeline, model training, evaluation, and visualization of results, including a accuracy_loss graph to assess model performance.
+
+## Baseline
+
+```bash
+python3 load_model.py
+```
---
+++ load_model.py
@@ -0,0 +1,80 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications import MobileNet
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_accuracy, top_k_categorical_accuracy, categorical_crossentropy
+from tensorflow.keras.applications.mobilenet import preprocess_input
+
+print("NumPy version:", np.__version__)
+print("TensorFlow version:", tf.__version__)
+
+import pandas as pd
+import cv2
+from PIL import Image
+
+cats = ('airplane', 'alarm clock', 'ambulance', 'angel', 'animal migration', 'ant', 'anvil', 'apple', 'arm', 'asparagus', 'axe', 'backpack', 'banana', 'bandage', 'barn', 'baseball', 'baseball bat', 'basket', 'basketball', 'bat', 'bathtub', 'beach', 'bear', 'beard', 'bed', 'bee', 'belt', 'bench', 'bicycle', 'binoculars', 'bird', 'birthday cake', 'blackberry', 'blueberry', 'book', 'boomerang', 'bottlecap', 'bowtie', 'bracelet', 'brain', 'bread', 'bridge', 'broccoli', 'broom', 'bucket', 'bulldozer', 'bus', 'bush', 'butterfly', 'cactus', 'cake', 'calculator', 'calendar', 'camel', 'camera', 'camouflage', 'campfire', 'candle', 'cannon', 'canoe', 'car', 'carrot', 'castle', 'cat', 'ceiling fan', 'cell phone', 'cello', 'chair', 'chandelier', 'church', 'circle', 'clarinet', 'clock', 'cloud', 'coffee cup', 'compass', 'computer', 'cookie', 'cooler', 'couch', 'cow', 'crab', 'crayon', 'crocodile', 'crown', 'cruise ship', 'cup', 'diamond', 'dishwasher', 'diving board', 'dog', 'dolphin', 'donut', 'door', 'dragon', 'dresser', 'drill', 'drums', 'duck', 'dumbbell', 'ear', 'elbow', 'elephant', 'envelope', 'eraser', 'eye', 'eyeglasses', 'face', 'fan', 'feather', 'fence', 'finger', 'fire hydrant', 'fireplace', 'firetruck', 'fish', 'flamingo', 'flashlight', 'flip flops', 'floor lamp', 'flower', 'flying saucer', 'foot', 'fork', 'frog', 'frying pan', 'garden', 'garden hose', 'giraffe', 'goatee', 'golf club', 'grapes', 'grass', 'guitar', 'hamburger', 'hammer', 'hand', 'harp', 'hat', 'headphones', 'hedgehog', 'helicopter', 'helmet', 'hexagon', 'hockey puck', 'hockey stick', 'horse', 'hospital', 'hot air balloon', 'hot dog', 'hot tub', 'hourglass', 'house', 'house plant', 'hurricane', 'ice cream', 'jacket', 'jail', 'kangaroo', 'key', 'keyboard', 'knee', 'ladder', 'lantern', 'laptop', 'leaf', 'leg', 'light bulb', 'lighthouse', 'lightning', 'line', 'lion', 'lipstick', 'lobster', 'lollipop', 'mailbox', 'map', 'marker', 'matches', 'megaphone', 'mermaid', 'microphone', 'microwave', 'monkey', 'moon', 'mosquito', 'motorbike', 'mountain', 'mouse', 'moustache', 'mouth', 'mug', 'mushroom', 'nail', 'necklace', 'nose', 'ocean', 'octagon', 'octopus', 'onion', 'oven', 'owl', 'paint can', 'paintbrush', 'palm tree', 'panda', 'pants', 'paper clip', 'parachute', 'parrot', 'passport', 'peanut', 'pear', 'peas', 'pencil', 'penguin', 'piano', 'pickup truck', 'picture frame', 'pig', 'pillow', 'pineapple', 'pizza', 'pliers', 'police car', 'pond', 'pool', 'popsicle', 'postcard', 'potato', 'power outlet', 'purse', 'rabbit', 'raccoon', 'radio', 'rain', 'rainbow', 'rake', 'remote control', 'rhinoceros', 'river', 'roller coaster', 'rollerskates', 'sailboat', 'sandwich', 'saw', 'saxophone', 'school bus', 'scissors', 'scorpion', 'screwdriver', 'sea turtle', 'see saw', 'shark', 'sheep', 'shoe', 'shorts', 'shovel', 'sink', 'skateboard', 'skull', 'skyscraper', 'sleeping bag', 'smiley face', 'snail', 'snake', 'snorkel', 'snowflake', 'snowman', 'soccer ball', 'sock', 'speedboat', 'spider', 'spoon', 'spreadsheet', 'square', 'squiggle', 'squirrel', 'stairs', 'star', 'steak', 'stereo', 'stethoscope', 'stitches', 'stop sign', 'stove', 'strawberry', 'streetlight', 'string bean', 'submarine', 'suitcase', 'sun', 'swan', 'sweater', 'swing set', 'sword', 't-shirt', 'table', 'teapot', 'teddy-bear', 'telephone', 'television', 'tennis racquet', 'tent', 'The Eiffel Tower', 'The Great Wall of China', 'The Mona Lisa', 'tiger', 'toaster', 'toe', 'toilet', 'tooth', 'toothbrush', 'toothpaste', 'tornado', 'tractor', 'traffic light', 'train', 'tree', 'triangle', 'trombone', 'truck', 'trumpet', 'umbrella', 'underwear', 'van', 'vase', 'violin', 'washing machine', 'watermelon', 'waterslide', 'whale', 'wheel', 'windmill', 'wine bottle', 'wine glass', 'wristwatch', 'yoga', 'zebra', 'zigzag')
+
+def top_3_accuracy(y_true, y_pred):
+    return top_k_categorical_accuracy(y_true, y_pred, k=3)
+
+def top_5_accuracy(y_true, y_pred):
+    return top_k_categorical_accuracy(y_true, y_pred, k=5)
+
+# def preds2catids(predictions):
+#     return pd.DataFrame(np.argsort(-predictions, axis=1)[:, :5], columns=['a', 'b', 'c', 'd', 'e'])
+
+def preds2catids(predictions):
+    top5_indices = np.argsort(-predictions, axis=1)[:, :5].astype(int)
+    top5_probs = np.take_along_axis(predictions, top5_indices, axis=1)
+
+    df = pd.DataFrame(top5_indices, columns=['a', 'b', 'c', 'd', 'e'])
+    df['probs'] = top5_probs.tolist()
+    
+    return df
+
+def draw_cv2(raw_strokes, size=256, lw=6, time_color=True):
+    BASE_SIZE = 256
+    img = np.zeros((BASE_SIZE, BASE_SIZE), np.uint8)
+    for t, stroke in enumerate(raw_strokes):
+        for i in range(len(stroke[0]) - 1):
+            color = 255 - min(t, 10) * 13 if time_color else 255
+            _ = cv2.line(img, (stroke[0][i], stroke[1][i]),
+                         (stroke[0][i + 1], stroke[1][i + 1]), color, lw)
+    if size != BASE_SIZE:
+        return cv2.resize(img, (size, size))
+    else:
+        return img
+
+def df_to_image_array_xd(df, size, lw=6, time_color=True):
+    # df['drawing'] = df['drawing'].apply(json.loads)
+    x = np.zeros((len(df), size, size, 1))
+    for i, raw_strokes in enumerate(df.drawing.values):
+        x[i, :, :, 0] = draw_cv2(raw_strokes, size=size, lw=lw, time_color=time_color)
+    x = preprocess_input(x).astype(np.float32)
+    return x
+
+NCATS = 340
+size = 64
+model = MobileNet(input_shape=(size, size, 1), alpha=1., weights=None, classes=NCATS)
+model.load_weights('model.h5')
+model.compile(optimizer=Adam(learning_rate=0.002), loss='categorical_crossentropy',
+              metrics=[categorical_crossentropy, categorical_accuracy, top_3_accuracy])
+
+print("## model loaded")
+
+df = pd.read_json('output.ndjson', lines=True)
+x_test = df_to_image_array_xd(df, size)
+prediction = model.predict(x_test)
+top3 = preds2catids(prediction)
+id2cat = {k: cat.replace(' ', '_') for k, cat in enumerate(cats)}
+top3cats = top3.replace(id2cat)
+
+print("## prediction")
+print(top3cats)
+# print(prediction)
+
+# model.export("tf_save") 
+# print("saved as save model")
+
+# then load:
+# tensorflowjs_converter --input_format tf_saved_model --output_format tfjs_graph_model --saved_model_tags serve tf_save/ tfjs
